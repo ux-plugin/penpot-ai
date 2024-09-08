d8785a5db462e9dd655cf0701022317a40002ce9
dhiaeddine <dhiaeddine.tounekti@sap.com>
2024-09-09 00:33:20 +0200

    plugin first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = createSignal(0)
+  function handleClick() {
+    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count: count() } }, '*')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div class='flex flex-col w-full h-full bg-slate-300 justify-center items-center'>
+      <h2>Rectangle Creator</h2>
+      <p>Count: <input id="count" value={count()} onChange={(event) => setCount(+event.target.value)} /></p>
+      <Button id="create" onClick={handleClick}>Create</Button>
+      <Button id="cancel" onClick={() => alert("canceled operation")}>Cancel</Button>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+});
